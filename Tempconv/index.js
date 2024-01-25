90b627a210659959ec6e0a3da8baec4c8cc3f895
Saswat <108627591+Saswattt@users.noreply.github.com>
2024-01-24 21:58:10 -0800

    Delete Tempconv directory

--- Tempconv/index.js
+++
@@ -1,9 +0,0 @@
-const Text = document.querySelector("#cel");
-const Btn = document.querySelector("button");
-const tText = document.querySelector("#far");
-Btn.addEventListener("click", () => {
-  let text = Text.value.trim();
-  let ans = text * (9 / 5) + 32;
-  console.log(ans);
-  tText.value = ans;
-});
